3fc55b7c8b9be77e616487de441bac7ddb0a3767
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-01-17 17:07:15 -0600

    Init

---
+++ hello-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello-ios
+//
+//  Created by Jerred Shepherd on 2019-01-17.
+//  Copyright © 2019 Jerred Shepherd. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
